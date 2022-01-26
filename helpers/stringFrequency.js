6088f45a10925f132f2374635dc40262accc2955
Wai Lon <rxdvd0@gmail.com>
2022-01-26 17:05:31 +0000

    fix regex parsing error

--- helpers/stringFrequency.js
+++ helpers/stringFrequency.js
@@ -1,7 +1,11 @@
+// https://stackoverflow.com/questions/3561493
+function escapeRegEx(string){
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
 
 // https://stackoverflow.com/questions/4009756
 function countOccurrences(substring, string){
-    let regex = new RegExp(substring, 'g');
+    let regex = new RegExp(escapeRegEx(substring), 'g');
     let count = (string.match(regex) || []).length;
     return count;
 }
