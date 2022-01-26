5520f704d51759a1ab1f834fcabeaadf0496a188
Wai Lon <rxdvd0@gmail.com>
2022-01-26 16:22:20 +0000

    add results loading functionality

--- client/searchResults.js
+++ client/searchResults.js
@@ -1,3 +1,13 @@
+const searchBar = document.querySelector('#searchbartext');
+
+searchBar.addEventListener('keypress', e => {
+    if(e.key === 'Enter' && e.target.value) {
+        window.location.search = `?q=${encodeURIComponent(e.target.value)}`;
+    }
+});
+
+loadResults();
+
 function parseURLQuery(){
     let query = location.search.slice(1);
     
@@ -18,9 +28,44 @@ function parseURLQuery(){
     return parsedQuery;
 }
 
+function clearResults(){
+    const resultsElement = document.querySelector('#searchresultsarea');
+    resultsElement.innerHTML = "";
+}
+
+function renderStats(data, startTime){
+    let reqTotalTime = (new Date()).getTime() - startTime;
+    let resultStats = document.createElement('p');
+    resultStats.id = 'searchresultsnumber';
+    resultStats.textContent = `About ${data.length} results (${ (reqTotalTime / 1e3).toFixed(2) } seconds)`;
+
+    const resultsElement = document.querySelector('#searchresultsarea');
+    resultsElement.insertBefore(resultStats, resultsElement.firstChild);
+
+}
+
 function renderData(data){
-    const resultsElement = document.querySelector('#results');
-    // for each data item create and append result
+    const resultsElement = document.querySelector('#searchresultsarea');
+    data.forEach(dataItem => {
+        let result = document.createElement('div');
+        let resultHeading = document.createElement('h2');
+        let resultURL = document.createElement('a');
+        let resultButton = document.createElement('button');
+        let resultPreview = document.createElement('p');
+
+        result.className = 'searchresult';
+        resultHeading.textContent = dataItem.title;
+        resultURL.href = dataItem.url;
+        resultURL.textContent = dataItem.url;
+        resultButton.textContent = '▼';
+        resultPreview.textContent = dataItem.preview;
+
+        result.appendChild(resultHeading);
+        result.appendChild(resultURL);
+        result.appendChild(resultButton);
+        result.appendChild(resultPreview);
+        resultsElement.appendChild(result);
+    });
 }
 
 function getData(searchInput, callback){
@@ -36,7 +81,13 @@ function loadResults(){
         window.location.href = "index.html";
     }
 
-    getData(query, renderData);
-}
+    document.querySelector('#searchbartext').value = decodeURIComponent(query);
 
-// loadResults();
+    let reqStartTime = new Date();
+
+    getData(query, data => {
+        clearResults();
+        renderData(data);
+        renderStats(data, reqStartTime);
+    });
+}
