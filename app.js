2867228ecdc03e2b1fd480fa58f9c5851321c31b
Wai Lon <rxdvd0@gmail.com>
2022-01-26 10:01:40 +0000

    add search route file

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+
+const searchRoutes = require('./routes/searchRoutes');
+
 app.use(cors());
-
-
-
+app.use(express.json());
+app.use('/search', searchRoutes);
 
 module.exports = app;
